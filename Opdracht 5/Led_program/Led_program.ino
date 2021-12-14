2dd3f710652740a77f8beb60c6c47a7ccb31dac1
vliegbanaan <56888959+vliegbanaan@users.noreply.github.com>
2021-12-14 10:15:23 +0100

    Update Led_program.ino
    Pin changed from 15 to make it work on my board.
    
    removed typo's

--- Opdracht 5/Led_program/Led_program.ino
+++ Opdracht 5/Led_program/Led_program.ino
@@ -2,13 +2,13 @@
 
 #define LED1 12  // pin 12
 #define LED2 13  // pin 13
-#define LED3+ 15  // pin 14
+#define LED3 14  // pin 14
 
 struct s_led {
   byte          gpio;         //LED GPIO NUMMER
   byte          state;        // LED STATE
   unsigned      napms;        // Delay to use(ms)
-  TaskHandle_t  task;         //task handle  
+  TaskHandle_t  taskh;         //task handle  
 };
 
 static s_led leds[3] = {
@@ -29,7 +29,7 @@ static void led_task_func(void *argp) {
     if ( !stack_hwm || temp < stack_hwm ) {
       stack_hwm = temp;
       printf("Task for gpio %d has stack hwm %u\n",
-        ledp->gpio,stack_hwm;
+        ledp->gpio,stack_hwm);
     }
     delay(ledp->napms);
   }
@@ -40,7 +40,7 @@ void setup() {
 
   delay(500);  //pause for serial setup
 
-  app_cpu = xPortGetCoreId();
+  app_cpu = xPortGetCoreID();
   printf("app_CPU is %d (%s core)\n",
   app_cpu,
   app_cpu > 0 ? "dual" : "Single");
